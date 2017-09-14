7da39ad408bc203900003d78cb76fe1aeb9f5ca7
Steven Sloan <stevenosloan@gmail.com>
2017-09-14 02:08:29 -0400

    feat: add convenience method for creating a new collection

--- src/Docurator.php
+++ src/Docurator.php
@@ -6,4 +6,15 @@ class Docurator
     {
         return true;
     }
+
+    public static function collection($src, $type, $overrides = [])
+    {
+        if (array_key_exists($type, Docurator\Configuration::BUILT_INS)) {
+            $config = Docurator\Configuration::builtIn($type, $overrides);
+        } else {
+            $config = new Docurator\Configuration($overrides);
+        }
+
+        return new Docurator\Collection($src, $config);
+    }
 }
