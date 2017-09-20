701daefda96a0f5a1920c8ee56a5d80d546f961d
Steven Sloan <stevenosloan@gmail.com>
2017-09-20 01:23:40 -0400

    tests: add rough test for Document scanning

---
+++ tests/Docurator/DocumentTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Docurator;
+
+use Docurator\Configuration;
+use Docurator\Document;
+
+use Tests\Util;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class DocumentTest extends TestCase
+{
+    public function setUp()
+    {
+        mkdir("tmp");
+    }
+
+    public function tearDown()
+    {
+        Util::delTree("tmp");
+    }
+
+    public function test_scan()
+    {
+        file_put_contents("tmp/test.less", <<<EOF
+/** doc
+hello: world
+*/
+.some_css {
+    display: none;
+}
+
+/** doc
+test:
+ - what
+ - a
+ - wonderful
+ - world
+*/
+EOF
+        );
+        $config  = Configuration::builtIn("less");
+        $file    = new \SplFileInfo("tmp/test.less");
+
+        $subject = new Document($config, $file);
+
+        $this->assertSame($config, $subject->config);
+        $this->assertSame($file, $subject->file_info);
+
+        $this->assertCount(2, $subject->blocks);
+        $this->assertContainsOnlyInstancesOf(Document\Block::class, $subject->blocks);
+
+        $first_block = $subject->blocks[0];
+        $second_block = $subject->blocks[1];
+
+        $this->assertSame(
+            ["hello" => "world"],
+            $first_block->data
+        );
+        $this->assertSame(0, $first_block->open_line_number);
+        $this->assertSame(2, $first_block->end_line_number);
+
+        $this->assertSame(
+            ["test" => ["what", "a", "wonderful", "world"]],
+            $second_block->data
+        );
+        $this->assertSame(7, $second_block->open_line_number);
+        $this->assertSame(13, $second_block->end_line_number);
+    }
+}
